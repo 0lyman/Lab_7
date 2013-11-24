03d2b02dd13d111b64f0392ba5b92024667b13f2
0lyman <ldolenslager@byu.net>
2013-11-24 02:16:48 -0700

    11/24/13 It all passed

--- Template_MysteryMap.h
+++ Template_MysteryMap.h
@@ -28,7 +28,7 @@ void addItem(I item)		// adds an item at the end of the vector of items
 I getRandomItem()			// returns a random item from the vector of items
 I getItems(int indexNumber)	// return item from vector at indicated index Number, else returns NULL
 vector<I> getAllItems()		// return the vector Items
-bool findItem(I item)		// search Items to find Item. if found return indexing number of its location in the vector else return -1
+int findItem(I item)		// search Items to find Item. if found return indexing number of its location in the vector else return -1
 void setKey(K key)			// set the value of Key
 K getKey()					// return the value of Key
 */
@@ -37,56 +37,56 @@ K getKey()					// return the value of Key
 template <class K, class I> class MysteryMap
 {
 public:
-	MysteryMap() {}
-	MysteryMap(K key, I item)
-	{
-		srand(time(NULL));
-		setKey(key);
-		Items.push_back(item);
-	}
-	~MysteryMap(){}
-	void addItem(I item)  // adds an item at the end of the vector of items
-	{
-		Items.push_back(item);
-	}
-	I getRandomItem()  // returns a random item from the vector of items
-	{
-		return Items[rand() % Items.size()];
-	}
-	vector<I> getAllItems() // return the vector Items
-	{
-		return Items;
-	}
-	I getItems(int indexNumber) // return item from vector at indicated index Number, else returns NULL
-	{
-		if (indexNumber < Items.size())
-			return Items[indexNumber];
-		else
-			return NULL;
-	}
-	int findItem(I item) // search Items to find Item. if found return indexing number of its location in the vector else return -1
-	{
-		if (Items.size() != 0)
-		{
-			for (int x=0; x < Items.size(); x++)
-			{
-				if (item == Items[x])
-					return x;
-			}
-		}
-		else
-			return -1;
-	}
-	void setKey(K key) // set the value of Key
-	{
-		Key = key;
-	}
-	K getKey() // return the value of Key
-	{
-		return Key;
-	}
+    MysteryMap() {}
+    MysteryMap(K key, I item)
+    {
+        srand(time(NULL));
+        setKey(key);
+        Items.push_back(item);
+    }
+    ~MysteryMap() {}
+    void addItem(I item)  // adds an item at the end of the vector of items
+    {
+        Items.push_back(item);
+    }
+    I getRandomItem()  // returns a random item from the vector of items
+    {
+        return Items[rand() % Items.size()];
+    }
+    vector<I> getAllItems() // return the vector Items
+    {
+        return Items;
+    }
+    I getItems(int indexNumber) // return item from vector at indicated index Number, else returns NULL
+    {
+        if (indexNumber < Items.size())
+            return Items[indexNumber];
+        else
+            return NULL;
+    }
+    int findItem(I item) // search Items to find Item. if found return indexing number of its location in the vector else return -1
+    {
+        if (Items.size() != 0)
+        {
+            for (int x=0; x < Items.size(); x++)
+            {
+                if (item == Items[x])
+                    return x;
+            }
+        }
+        else
+            return -1;
+    }
+    void setKey(K key) // set the value of Key
+    {
+        Key = key;
+    }
+    K getKey() // return the value of Key
+    {
+        return Key;
+    }
 protected:
-	K Key;
-	vector<I> Items;  // vector of like items assosiated with the key
+    K Key;
+    vector<I> Items;  // vector of like items assosiated with the key
 };
 
