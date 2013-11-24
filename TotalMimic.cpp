bc7ebafce45c10dc0721e2bbe0f2b3461d7f6016
0lyman <ldolenslager@byu.net>
2013-11-24 02:15:41 -0700

    11/24/13 It all passed

--- TotalMimic.cpp
+++ TotalMimic.cpp
@@ -3,7 +3,7 @@
 
 TotalMimic::TotalMimic()
 {
-    testBit = true; // true for testing, false otherwise
+    testBit = false; // true for testing, false otherwise
 }
 
 
@@ -45,9 +45,9 @@ TotalMimic::~TotalMimic()
 *  with the suffix "THE_END".
 *
 * @param input
-* 		the sample text to be mimicked
+*                 the sample text to be mimicked
 */
-void TotalMimic :: createMap(string input)
+void TotalMimic::createMap(string input)
 {
     istringstream ss;
     ss.str(input);
@@ -55,25 +55,25 @@ void TotalMimic :: createMap(string input)
     string word_two;
     string word_three;
     ss >> word_one;  // get first three words
-    if(testBit)
+    if (testBit)
     {
-        cout << "here is the input: " << input<< endl;
+        cout << "here is the input: " << input << endl;
     }
-    if(testBit)
+    if (testBit)
     {
-        cout << "here is the first word: " << word_one<< endl;
+        cout << "here is the first word: " << word_one << endl;
     }
     ss >> word_two;
-    if(testBit)
+    if (testBit)
     {
-        cout << "here is the second word: " << word_two<< endl;
+        cout << "here is the second word: " << word_two << endl;
     }
     ss >> word_three;
-    if(testBit)
+    if (testBit)
     {
         cout << "here is the third word: " << word_three << endl;
     }
-    if(!ss.good())
+    if (!ss.good())
     {
 
     }
@@ -83,52 +83,79 @@ void TotalMimic :: createMap(string input)
         istringstream PassMe;
         PassMe.str(word_one + " " + word_two);
         string Key = PassMe.str();
-        if(testBit)
+        if (testBit)
         {
             cout << "here is a Prefix 0: " << Key << endl;
-            cout << "here is a Suffix 0: " << word_three<< endl;
+            cout << "here is a Suffix 0: " << word_three << endl;
         }
-        Prefixes.push_back(new MysteryMap<string, string>(Key, word_three));  // first two become prefix and the third the suffix
+
+        if (!doesPrefixExist(Key))
+        {
+            Prefixes.push_back(new MysteryMap<string, string>(Key, word_three));// first two become prefix and the third the suffix
+        }
+        else  // if prefix allready exists add suffix to it
+        {
+            Prefixes[getPrefixLocation(Key)]->addItem(word_three);
+        }
         word_one.clear();
         word_one = word_two; // shift the words foreward into one and two
-        if(testBit)
+        if (testBit)
         {
-            cout << "here is a word 1: " << word_one<< endl;
+            cout << "here is a word 1: " << word_one << endl;
         }
         word_two.clear();
         word_two = word_three;
-        if(testBit)
+        if (testBit)
         {
-            cout << "here is a word 2: " << word_two<< endl;
+            cout << "here is a word 2: " << word_two << endl;
         }
-        if(!ss.good())  // check if there are not more words in the string input
+        if (!ss.good())  // check if there are not more words in the string input
         {
             istringstream Passer;   // if no more words last two wors are the prefix and it is the end
             Passer.str(word_one + " " + word_two);
             string Key = Passer.str();
             string suffix = "THE_END";
-            if(testBit)
+            if (testBit)
             {
-                cout << "here is a Prefix 1: " << Key<< endl;
-                cout << "here is a Suffix 1: " << suffix<< endl;
+                cout << "key/Prefix before storage: "<< Key << endl;
+                cout << "Suffix before storage: "<< suffix << endl;
+                cout << "last Prefix" << endl;
             }
-            
-            Prefixes.push_back(new MysteryMap<string, string>(Key, suffix));
+            if (!doesPrefixExist(Key))
+            {
+                if (testBit)
+                {
+                    cout << "here is a Prefix 1: " << Key << endl;
+                    cout << "here is a Suffix 1: " << suffix << endl;
+                }
+                Prefixes.push_back(new MysteryMap<string, string>(Key, suffix));
+            }
+            else // if prefix allready exists add suffix to it
+            {
+                if (testBit)
+                {
+                    cout << "here is a Prefix 2: " << Key << endl;
+                    cout << "here is a Suffix 2: " << suffix << endl;
+                }
+                Prefixes[getPrefixLocation(Key)]->addItem(suffix);
+            }
             Go = false;
         }
         if (ss.good()) // check if there are more words in the string input
         {
             word_three.clear();
             ss >> word_three;   // get a new word for the third
-            if(testBit)
+            if (testBit)
             {
-                cout << "here is a word 3: " << word_three<< endl;
+                cout << "here is a word 3: " << word_three << endl;
             }
         }
     }
     while (Go);
-    // read in
-    // while loop saves the suffix to the map of the prefix, if new prefix it creates a map and reassigns the prefix
+    if(testBit)
+    {
+        cout << " End read in "<<endl;
+    }
 }
 
 /**
@@ -139,9 +166,9 @@ void TotalMimic :: createMap(string input)
 * map has been created yet.
 *
 * @param prefix
-* 		the prefix to be found
+*                 the prefix to be found
 * @return a list of suffixes associated with the given prefix if the
-* 		prefix is found; an empty vector otherwise
+*                 prefix is found; an empty vector otherwise
 */
 vector<string> TotalMimic::getSuffixList(string prefix)
 {
@@ -169,7 +196,7 @@ vector<string> TotalMimic::getSuffixList(string prefix)
 * "THE_END" should not be returned as part of your generated text.
 *
 * @return random text generated using the map created with the sample
-* 		text; an empty string if no map has been created yet
+*                 text; an empty string if no map has been created yet
 */
 string TotalMimic::generateText()
 {
@@ -184,41 +211,41 @@ string TotalMimic::generateText()
         string firstHalfPrefix;
         string seccondHalfPrefix;
         GeneratedText = Prefixes[0]->getKey();  // set first words as the first Prefix
-        if(testBit)
+        if (testBit)
         {
-            cout << "GeneratedText here 00: " << GeneratedText<< endl;
+            cout << "GeneratedText here 00: " << GeneratedText << endl;
         }
         istringstream Splitter;
         Splitter.str(Prefixes[0]->getKey());  // split the prefix into two words
         Splitter >> junk;
-        if(testBit)
+        if (testBit)
         {
-            cout << "junk here 00: " << junk<< endl;
+            cout << "junk here 00: " << junk << endl;
         }
         Splitter >> firstHalfPrefix;
-        if(testBit)
+        if (testBit)
         {
-            cout << "firstHalfPrefix here 00: " << firstHalfPrefix<< endl;
+            cout << "firstHalfPrefix here 00: " << firstHalfPrefix << endl;
         }
         seccondHalfPrefix = Prefixes[0]->getRandomItem();  // get a random suffix to put into the GeneratedText and use as the seccond half of the next prefix
-        if(testBit)
+        if (testBit)
         {
-            cout << "seccondHalfPrefix here 00: " << seccondHalfPrefix<< endl;
+            cout << "seccondHalfPrefix here 00: " << seccondHalfPrefix << endl;
         }
         if (seccondHalfPrefix == "THE_END") // if the end return the GeneratedText
             return GeneratedText;
         else
         {
             GeneratedText = GeneratedText + " " + seccondHalfPrefix;   // if not "THE_END" put it into the GeneratedText string
-            if(testBit)
+            if (testBit)
             {
-                cout << "GeneratedText here 01: " << GeneratedText<< endl;
+                cout << "GeneratedText here 01: " << GeneratedText << endl;
             }
             NextPrefix.clear();
             NextPrefix = firstHalfPrefix + " " + seccondHalfPrefix;  // combine to firstHalfPrefix and seccondHalfPrefix to find the next Prefix
-            if(testBit)
+            if (testBit)
             {
-                cout << "NextPrefix here 01: " << NextPrefix<< endl;
+                cout << "NextPrefix here 01: " << NextPrefix << endl;
             }
             nextPrefixIndex = getPrefixLocation(NextPrefix);
         }
@@ -245,6 +272,7 @@ string TotalMimic::generateText()
 /*
 Finds given Prefix (findPrefix) and returns index number
 of the location in the vector Prefixes where Prefix is located
+else return -1 if not found
 */
 int TotalMimic::getPrefixLocation(string findPrefix)
 {
@@ -253,4 +281,20 @@ int TotalMimic::getPrefixLocation(string findPrefix)
         if (findPrefix == Prefixes[x]->getKey())
             return x;
     }
+}
+
+/*
+used to decide if Prefix allread exists or not
+*/
+bool TotalMimic::doesPrefixExist(string findPrefix)
+{
+    if (Prefixes.size() != 0)
+    {
+        for (int x=0; x < Prefixes.size(); x++)
+        {
+            if (findPrefix == Prefixes[x]->getKey())
+                return true;
+        }
+    }
+    return false;
 }
