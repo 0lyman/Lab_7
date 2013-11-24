6ed7f4114704be4e9784574a8ada22cf81667223
Dane Olenslager <ldolenslager@byu.net>
2013-11-23 19:14:42 -0700

    11/23/13
    I think its done, get a fork...

--- Template_MysteryMap.h
+++ Template_MysteryMap.h
@@ -4,48 +4,54 @@
 #include <sstream>  
 #include <cctype>
 #include <vector>
+#include <time.h>
+#include <stdlib.h>     /* srand, rand */
 
 using namespace std;
 
 template <class K, class I> class MysteryMap
 {
 public:
-	MysteryMap()
+	MysteryMap() {}
+	MysteryMap(K key, I item)
 	{
-
+		srand(time(NULL));
+		Key = key;
+		Items.push_back(item);
 	}
-	MysteryMap(K Key)
+	~MysteryMap(){}
+	void addItem(I item)
 	{
-
-	}
-//	MysteryMap(K Key, I Item);
-	~MysteryMap()
-	{
-
-	}
-	void addItem(I Item)
-	{
-
-	}
-	I getItem()
-	{
-
+		Items.push_back(item);
 	}
 	I getRandomItem()
 	{
-
+		return Items[rand() % Items.size()]
 	}
-	vector<I> getAllItems()
+	vector<I> getAllItems() // return the vector Items
 	{
-
+		return Items;
 	}
-	K setKey(K Key)
+	void setKey(K key) // set the value of Key
 	{
-
+		Key = key;
 	}
-	K getKey()
+	K getKey() // return the value of Key
 	{
-
+		return Key;
+	}
+	bool findItem(I item) // search Items to find Item. if found return true else return false
+	{
+		if (Items.size() != 0)
+		{
+			for (int x=0; x < Items.size(); x++)
+			{
+				if (item == Items[x])
+					return true;
+			}
+		}
+		else
+			return false;
 	}
 protected:
 	K key;
